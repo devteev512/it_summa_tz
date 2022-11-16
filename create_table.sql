e44838d09259b2935ade1aae00825ada5b28f525
devteev512 <ferettel64@mail.ru>
2022-11-17 02:23:07 +0300

    my_tz

---
+++ create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE public.test_evteev2 (
+	id int4 NULL,
+	firstname varchar(200) NULL,
+	lastname varchar(200) NULL,
+	phone varchar(200) NULL,
+	email varchar(200) NULL
+)distributed by (id);
